4860ea52474ff784a4d61d7125fc4fdaa0049a36
unknown <safwanalqulayti@gmail.com>
2021-08-16 21:42:48 +0300

    ng prime

--- Restaurant-Owner/src/app/app.module.ts
+++ Restaurant-Owner/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    SignUpComponent
+    SignUpComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ButtonModule,
+    BrowserAnimationsModule
+    
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Restaurant-Owner/src/app/log-in/log-in.component.ts
+++ Restaurant-Owner/src/app/log-in/log-in.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
+  templateUrl: 'log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private primengConfig:PrimeNGConfig
+  ) { }
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
 
 }
