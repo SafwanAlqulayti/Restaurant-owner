054b1a2be9bbf8da1f5737feeafbd8c6cfa5ff9d
unknown <safwanalqulayti@gmail.com>
2021-08-17 21:19:45 +0300

    test

--- Restaurant-Owner/src/app/log-in/log-in.component.ts
+++ Restaurant-Owner/src/app/log-in/log-in.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, Output, EventEmitter,OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -7,11 +8,22 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
   constructor(
     private primengConfig:PrimeNGConfig
   ) { }
+  submit() {
+  if (this.form.valid) {
+    this.submitEM.emit(this.form.value);
+  }
+}
+@Input()
+ error: string | null | undefined;
 
+@Output() submitEM = new EventEmitter();
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
